65a5892c9a13f9c84030688b94b7e3eb26321129
pcannon09 <114769055+pcannon09@users.noreply.github.com>
2024-01-12 20:28:42 +0100

    Upload main.cpp

---
+++ test/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#include "../inc/dt.hpp"
+
+int main()
+{
+    std::cout << "-= DateTime " << dt::version << " =-\n";
+    std::cout << "* By Paul Cannon\n";
+
+    dt::sysClock timeNow;
+
+    while (1)
+    {
+        timeNow = dt::get();
+        
+        dt::set24h(false);
+
+        std::cout << "Time: " << dt::hr(timeNow) << " ( " << dt::amOrPm(timeNow) << " ) : " << dt::min(timeNow) << " : " << dt::sec(timeNow) << " . " << dt::ms(timeNow) << " . " << dt::us(timeNow) << "\n";
+
+        dt::set24h(true);
+
+        std::cout << "Time: " << dt::hr(timeNow) << " : " << dt::min(timeNow) << " : " << dt::sec(timeNow) << " . " << dt::ms(timeNow) << " . " << dt::us(timeNow) << "\n";
+        std::cout << "Date (DD / MM / YYYY): " << dt::day() << " / " << dt::month() << " / " << dt::year() << "; Timezone " << dt::timezone() << "\n";
+        std::cout << "Date (More info)" << " Day of week: " << dt::wday() << " Day of year: " << dt::yday() << " DST: " << dt::isdst() << "\n";
+
+        system("clear");
+    }
+
+    return 0;
+}
+
