ad5306f4cea8cac32783ef3cee986ba247c461da
pcannon09 <114769055+pcannon09@users.noreply.github.com>
2024-01-13 12:14:16 +0100

    Add files via upload

--- test/main.cpp
+++ test/main.cpp
@@ -15,11 +15,11 @@ int main()
         
         dt::set24h(false);
 
-        std::cout << "Time: " << dt::hr(timeNow) << " ( " << dt::amOrPm(timeNow) << " ) : " << dt::min(timeNow) << " : " << dt::sec(timeNow) << " . " << dt::ms(timeNow) << " . " << dt::us(timeNow) << "\n";
+        std::cout << "Time: " << dt::hr(timeNow) << " ( " << dt::amOrPm(timeNow) << " ) : " << dt::min() << " : " << dt::sec(timeNow) << " . " << dt::ms(timeNow) << " . " << dt::us(timeNow) << "\n";
 
         dt::set24h(true);
 
-        std::cout << "Time: " << dt::hr(timeNow) << " : " << dt::min(timeNow) << " : " << dt::sec(timeNow) << " . " << dt::ms(timeNow) << " . " << dt::us(timeNow) << "\n";
+        std::cout << "Time: " << dt::hr(timeNow) << " : " << dt::min() << " : " << dt::sec(timeNow) << " . " << dt::ms(timeNow) << " . " << dt::us(timeNow) << "\n";
         std::cout << "Date (DD / MM / YYYY): " << dt::day() << " / " << dt::month() << " / " << dt::year() << "; Timezone " << dt::timezone() << "\n";
         std::cout << "Date (More info)" << " Day of week: " << dt::wday() << " Day of year: " << dt::yday() << " DST: " << dt::isdst() << "\n";
 
